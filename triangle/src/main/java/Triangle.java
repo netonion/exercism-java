f7c7f25751a0a27a569b45ca7435e68666cf626b
Sam Guo <netonion@hotmail.com>
2017-09-23 20:45:15 -0400

    Triangle without sort

--- triangle/src/main/java/Triangle.java
+++ triangle/src/main/java/Triangle.java
@@ -1,20 +1,17 @@
-import java.util.Arrays;
-
 class Triangle {
 
     private final TriangleKind kind;
 
     Triangle(double side1, double side2, double side3) throws TriangleException {
-        double[] sides = {side1, side2, side3};
-        Arrays.sort(sides);
-
-        if (sides[0] + sides[1] <= sides[2]) {
+        if (side1 + side2 <= side3 ||
+            side2 + side3 <= side1 ||
+            side3 + side1 <= side2) {
             throw new TriangleException();
         }
 
-        if (sides[0] == sides[1] && sides[1] == sides[2]) {
+        if (side1 == side2 && side2 == side3) {
             kind = TriangleKind.EQUILATERAL;
-        } else if (sides[0] == sides[1] || sides[1] == sides[2]) {
+        } else if (side1 == side2 || side2 == side3 || side1 == side3) {
             kind = TriangleKind.ISOSCELES;
         } else {
             kind = TriangleKind.SCALENE;
